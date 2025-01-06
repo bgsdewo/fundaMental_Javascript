f25ca5d4ef4d49acb990d619e92c2783c05f2582
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 09:48:18 +0700

    Variabel dalam Javascript

---
+++ JS_Dasar/variable.js
@@ -0,0 +1,26 @@
+//variabel dan kata kunci let
+// variabel adalah tempat untuk menyimpan data
+// variabel di javascript menggunakan kata kunci var, let, const
+// let digunakan untuk membuat variabel yang nilainya bisa diubah
+// const digunakan untuk membuat variabel yang nilainya tidak bisa diubah
+// var digunakan untuk membuat variabel yang nilainya bisa diubah
+
+// contoh penggunaan let
+let nama = "Dian";
+console.log(nama);
+nama = "Maulana"; // asign nilai baru pada variabel nama
+console.log(nama);
+
+// contoh dari operasi matematika
+let angka = 10;
+console.log(angka);
+angka = angka + 5;
+console.log(angka);
+angka += 5; // maksud dari ini adalah angka = angka + 5
+console.log(angka);
+angka *= 2;
+
+//contoh penggunaan const
+const nilai = 10;
+console.log(nilai);
+// nilai = 20; // akan error karena nilai tidak bisa diubah
