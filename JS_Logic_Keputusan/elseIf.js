f92006620dbdbf26d84a9649ea1d2e80d1c8227c
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 17:55:12 +0700

    Else If Statement Makin Banyak Pilihan  dan Else

---
+++ JS_Logic_Keputusan/elseIf.js
@@ -0,0 +1,16 @@
+//Else If Statement Makin Banyak Pilihan
+// Else If Statement adalah sebuah statement percabangan lanjutan yang digunakan
+// setelah if statement.
+// Else If Statement digunakan untuk memberikan kondisi lain yang akan dilakukan jika
+// kondisi pada if statement bernilai salah.
+// Contoh:
+
+// var x = 10;
+// if(x > 5){
+//     console.log('x lebih besar dari 5');
+// } else if(x == 5){
+//     console.log('x sama dengan 5');
+// } else {
+//     console.log('x lebih kecil dari 5');
+// }
+// console.log('Program Selesai');
