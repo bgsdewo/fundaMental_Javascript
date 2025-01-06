b26e5c160794f576a2d853df34d23473ab07d845
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 21:21:27 +0700

    Operator Logic And

---
+++ JS_Logic_Keputusan/operatorAnd.js
@@ -0,0 +1,21 @@
+// Operator Logic And
+// Operator logika AND (&&) digunakan untuk menggabungkan dua kondisi atau lebih.
+// Operator logika AND akan menghasilkan nilai true jika semua kondisi yang
+// diberikan bernilai true.
+// Contoh:
+// var x = 10;
+// var y = 5;
+// console.log(x > 5 && y < 10); // true
+// console.log(x > 5 && y > 10); // false
+// console.log(x < 5 && y < 10); // false
+// console.log(x < 5 && y > 10); // false
+// console.log(x > 5 && y == 5); // true
+
+// //contoh lain
+// var x = 10;
+// var y = 5;
+// if (x > 5 && y < 10) {
+//   console.log("x lebih besar dari 5 dan y lebih kecil dari 10");
+// } else {
+//   console.log("salah satu atau kedua kondisi tidak terpenuhi");
+// }
