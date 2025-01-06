64a07babbc3382e5555637e04a8011e0eeca111e
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 17:47:37 +0700

    If Statement Pertama Yg Kamu Pelajari

---
+++ JS_Logic_Keputusan/if.js
@@ -0,0 +1,10 @@
+// If Statement Pertama Yg Kamu Pelajari
+// If Statement adalah sebuah statement percabangan yang akan mengeksekusi blok kode jika
+// kondisi yang diberikan bernilai benar (true).
+// If Statement akan mengeksekusi blok kode jika kondisi bernilai benar,
+// jika kondisi bernilai salah maka blok kode tidak akan dieksekusi.
+// Contoh:
+// var x = 10;
+// if(x > 5){
+//     console.log('x lebih besar dari 5');
+// }
