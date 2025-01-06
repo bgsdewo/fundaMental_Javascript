363e85177c6a8161844e14d902689b06d460acad
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 09:04:46 +0700

    Tipe Data Numbers

---
+++ JS_Dasar/numbers.js
@@ -0,0 +1,15 @@
+// sub topic kedua Tipe Data Numbers
+// numbers dalam javascript adalah tipe data yang digunakan untuk menyimpan nilai numerik
+// didalam javascript terdapat dua tipe data numbers yaitu integer dan float
+// integer adalah bilangan bulat sedangkan float adalah bilangan desimal
+// memiliki bilangan negatif dan positif
+// contoh penggunaan bilangan negatif dan positif
+let positif = 10;
+let negatif = -10;
+console.log(positif);
+console.log(negatif);
+// contoh penggunaan numbers
+let integer = 10;
+let float = 10.5;
+console.log(integer);
+console.log(float);
