26a546b09224aac6b780828a1baeabce6896f2a8
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 09:16:05 +0700

    Apa itu NaN

---
+++ JS_Dasar/NaN.js
@@ -0,0 +1,9 @@
+// Apa itu nilai NaN?
+// NaN adalah singkatan dari Not a Number.
+// NaN adalah nilai spesial yang menunjukkan hasil operasi matematika yang tidak valid.
+// Misalnya hasil pembagian nol dengan nol, hasil operasi matematika yang melibatkan nilai yang tidak bisa diubah menjadi bilangan,
+// dan sebagainya.
+// contoh :
+console.log(10 / "Halo"); // NaN
+console.log(100 / 0); // Infinity
+console.log(0 / 0); // NaN
