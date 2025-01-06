da25044e08f3b1a39bc8bc94fe119f3a31490435
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 08:51:26 +0700

    ipe data primitif dan developer tools console

---
+++ JS_Dasar/script.js
@@ -0,0 +1,17 @@
+// sub topic pertama tipe data primitive pada JS
+// 1. String
+// string adalah tipe data yang berisi karakter atau teks
+// 2. Number
+// number adalah tipe data yang berisi angka
+// 3. Boolean
+// boolean adalah tipe data yang berisi nilai benar atau salah
+// 4. Null
+// null adalah tipe data yang berisi nilai kosong
+// 5. Undefined
+// undefined adalah tipe data yang berisi nilai yang belum didefinisikan
+// 6. Symbol
+// symbol adalah tipe data yang berisi nilai yang unik dan tidak bisa diubah
+// 7. BigInt
+// bigint adalah tipe data yang berisi angka yang sangat besar
+
+// kepanjangan repl adalah read evaluate print loop
