4f14e100cb83c15437183dd5a43defba9ffdb5e9
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 17:34:56 +0700

    Console Alert Dan Prompt

---
+++ JS_Logic_Keputusan/console.js
@@ -0,0 +1,11 @@
+//Console Alert Dan Prompt
+//alert adalah fungsi yang digunakan untuk menampilkan pesan kepada pengguna
+//contoh
+//alert('Hello World');
+//prompt adalah fungsi yang digunakan untuk meminta input dari pengguna
+//contoh
+//prompt('Masukkan nama anda');
+//contoh penggunaan alert dan prompt
+// var nama = prompt("Masukkan nama anda");
+// alert("Hello " + nama);
+// console.log(nama);
