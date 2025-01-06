1131a71721c6587cc8d68a0c919343841074ae47
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 17:01:17 +0700

    Mengenal Null Dan Undefined

---
+++ JS_Dasar/nullNUndefined.js
@@ -0,0 +1,16 @@
+//Mengenal Null Dan Undefined
+//null dan undefined adalah tipe data yang berbeda
+//null adalah nilai yang tidak ada
+//undefined adalah nilai yang belum didefinisikan
+//contoh
+
+// let nama = null;
+// console.log(nama); //null
+
+// let umur;
+// console.log(umur); //undefined
+// //null adalah nilai yang tidak ada
+// //undefined adalah nilai yang belum didefinisikan
+// //null adalah tipe data yang berbeda dengan undefined
+// //null adalah nilai yang tidak ada
+// //undefined adalah nilai yang belum didefinisikan
