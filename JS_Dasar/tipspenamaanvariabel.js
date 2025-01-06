4e1236162a30940ff3a61c937de9cf0b766461d5
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 10:02:25 +0700

    Tips Memberikan Nama Variabel Seperti Anak Sendiri

---
+++ JS_Dasar/tipspenamaanvariabel.js
@@ -0,0 +1,33 @@
+// Tips Memberikan Nama Variabel Seperti Anak Sendiri
+// Berikut adalah tips yang bisa kamu gunakan untuk memberikan nama variabel:
+// 1. Gunakan nama yang jelas
+// 2. Gunakan camelCase
+// 3. Gunakan nama yang singkat
+// 4. Hindari penggunaan nama yang sudah ada di JavaScript
+// 5. Gunakan nama yang mudah diingat
+// 6. Gunakan nama yang mudah diucapkan
+// 7. Gunakan nama yang mudah ditulis
+// 8. Gunakan nama yang konsisten
+// 9. Gunakan nama yang mudah diubah
+// 10. Gunakan nama yang mudah dihapus
+// 11. Gunakan nama yang mudah diubah menjadi kata benda
+// 12. Gunakan nama yang mudah diubah menjadi kata kerja
+// 13. Gunakan nama yang mudah diubah menjadi kata sifat
+// 14. Gunakan nama yang mudah diubah menjadi kata keterangan
+// 15. Gunakan nama yang mudah diubah menjadi kata ganti
+// 16. Gunakan nama yang mudah diubah menjadi kata bilangan
+// 17. Gunakan nama yang mudah diubah menjadi kata tanya
+// 18. Gunakan nama yang mudah diubah menjadi kata seru
+// 19. Gunakan nama yang mudah diubah menjadi kata hubung
+// 20. Gunakan nama yang mudah diubah menjadi kata depan
+//dan masih banyak lagi tips yang bisa kamu gunakan untuk memberikan nama variabel yang baik.
+
+//contoh penggunaaan penamaan variabel yang baik
+let namaLenkap = "Camel Case"; //L camelCase
+let NamaLengkap = "Pascal Case"; //PascalCase
+let nama_lenkap = "Snake Case"; //snake_case
+let $variable = "Dollar Case"; //DollarCase
+let _variable = "Underscore Case"; //UnderscoreCase
+let nama = "Dian"; //tidak disarankan
+let a = 10; //tidak disarankan
+let angka = 10; //disarankan
