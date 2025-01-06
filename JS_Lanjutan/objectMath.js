b8526492eb7e9589ccef902fac64b6d36dfd67f5
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 17:11:42 +0700

    Mengenal Object Math Dan Angka Acak

---
+++ JS_Lanjutan/objectMath.js
@@ -0,0 +1,46 @@
+//Mengenal Object Math Dan Angka Acak
+// Object Math adalah sebuah object bawaan dari JavaScript yang
+// menyediakan berbagai macam fungsi matematika.
+
+// 1. Math.PI
+// Math.PI adalah sebuah properti yang berisi nilai phi (π)
+
+// 2. Math.abs()
+// Math.abs() adalah sebuah fungsi yang digunakan untuk mengembalikan nilai absolut
+// dari sebuah angka.
+
+// 3. Math.pow()
+// Math.pow() adalah sebuah fungsi yang digunakan untuk mengembalikan hasil dari  angka
+// pangkat tertentu. Fungsi ini menerima dua parameter, yaitu angka yang akan dipangkatkan
+// dan pangkat dari angka tersebut.
+
+// 4. Math.sqrt()
+// Math.sqrt() adalah sebuah fungsi yang digunakan untuk mengembalikan nilai akar
+// dari sebuah angka.
+
+// 5. Math.round()
+// Math.round() adalah sebuah fungsi yang digunakan untuk membulatkan sebuah angka
+// ke bilangan bulat terdekat.
+
+// 6. Math.random()
+// Math.random() adalah sebuah fungsi yang digunakan untuk mengembalikan sebuah angka
+
+//7. Math.floor()
+// Math.floor() adalah sebuah fungsi yang digunakan untuk membulatkan sebuah angka
+// ke bawah ke bilangan bulat terdekat.
+
+//8. Math.ceil()
+// Math.ceil() adalah sebuah fungsi yang digunakan untuk membulatkan sebuah angka
+// ke atas ke bilangan bulat terdekat.
+
+//dan masih banyak lagi
+
+//misalnya
+console.log(Math.PI);
+console.log(Math.abs(-4.7));
+console.log(Math.pow(8, 2));
+console.log(Math.sqrt(64));
+console.log(Math.round(4.7));
+console.log(Math.random());
+console.log(Math.floor(4.7));
+console.log(Math.ceil(4.4));
