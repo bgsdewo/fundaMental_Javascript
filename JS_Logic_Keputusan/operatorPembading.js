c2b4ccbaa4252d5342fc893bd58e833ba007560a
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 17:20:40 +0700

    Operator Pembanding

---
+++ JS_Logic_Keputusan/operatorPembading.js
@@ -0,0 +1,26 @@
+// Operator Pembanding
+// Operator pembanding digunakan untuk membandingkan dua nilai.
+// Operator pembanding menghasilkan nilai boolean, yaitu true atau false.
+// Operator pembanding digunakan untuk membandingkan dua nilai.
+
+//misalnya
+// ==	: Sama dengan
+// !=	: Tidak sama dengan
+// >	: Lebih besar dari
+// <	: Lebih kecil dari
+// >=	: Lebih besar atau sama dengan
+// <=	: Lebih kecil atau sama dengan
+// ===	: Sama dengan dan tipe data sama
+// !==	: Tidak sama dengan dan tipe data sama
+// Contoh:
+
+// var x = 10;
+// var y = 5;
+// console.log(x == y); // false
+// console.log(x != y); // true
+// console.log(x > y); // true
+// console.log(x < y); // false
+// console.log(x >= y); // true
+// console.log(x <= y); // false
+// console.log(x === y); // false
+// console.log(x !== y); // true
