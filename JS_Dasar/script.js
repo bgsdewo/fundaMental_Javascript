c99484fb3bbd97bfb4053704724939318a24357f
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-07 06:18:22 +0700

    menambahkan penggunaan dari sebuah tipe data

--- JS_Dasar/script.js
+++ JS_Dasar/script.js
@@ -15,3 +15,37 @@
 // bigint adalah tipe data yang berisi angka yang sangat besar
 
 // kepanjangan repl adalah read evaluate print loop
+
+//contooh penggunaan tipe data string
+let nama = "Dian";
+console.log(nama);
+
+//contoh penggunaan tipe data number
+let angka = 10;
+console.log(angka);
+
+//contoh penggunaan tipe data boolean
+let benar = true;
+let salah = false;
+console.log(benar);
+console.log(salah);
+
+//contoh penggunaan tipe data null
+let kosong = null;
+console.log(kosong);
+kosong = "tidak kosong"; //asign nilai baru pada variabel kosong
+console.log(kosong); //output tidak kosong
+
+//contoh penggunaan tipe data undefined
+let belumDidefinisikan;
+console.log(belumDidefinisikan);
+belumDidefinisikan = "Sudah didefinisikan";
+console.log(belumDidefinisikan);
+
+//contoh penggunaan tipe data symbol
+let id = Symbol("id");
+console.log(id);
+
+//contoh penggunaan tipe data bigint
+let bigNumber = 1234567890123456789012345678901234567890n;
+console.log(bigNumber);
