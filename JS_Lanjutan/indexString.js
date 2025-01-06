4e65846f2a0f9ed8cba238bc8c14d6ab895e769b
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 11:42:47 +0700

    Penjelasan Index Pada String Dan Panjang Karakter

---
+++ JS_Lanjutan/indexString.js
@@ -0,0 +1,16 @@
+// Penjelasan Index Pada String Dan Panjang Karakter
+//string memiliki index atau posisi karakter
+//index string dimulai dari 0
+//contoh
+// let nama = "andi";
+// console.log(nama[0]); //a
+// console.log(nama[1]); //n
+// console.log(nama[2]); //d
+// console.log(nama[3]); //i
+// console.log(nama[4]); //undefined
+
+//menghitung panjang karakter
+// let panjang = nama.length;
+// console.log(panjang); //4
+// //mengambil karakter terakhir
+// console.log(nama[panjang - 1]); //i
