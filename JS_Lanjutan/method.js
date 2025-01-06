29907ee1e5b1b037589ce63cb0eac3c7d694d8cf
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 13:22:52 +0700

    beberapa method dalam JS

---
+++ JS_Lanjutan/method.js
@@ -0,0 +1,28 @@
+//Method-Method Javascript Yang Memudahkan Hidup Kita
+//string methods
+//method adalah fungsi yang dimiliki oleh tipe data
+//sangat membantu dalam memanipulasi data
+//misal
+// let nama = "andi";
+// //mengubah menjadi huruf besar
+// console.log(nama.toUpperCase()); //ANDI
+// //mengubah menjadi huruf kecil
+// console.log(nama.toLowerCase()); //andi
+
+// Method Dan Argument (Parameter)
+// method bisa memiliki argument atau parameter
+// argument adalah data yang di perlukan oleh method
+// contoh
+// let nama = "andi";
+// //mengganti karakter
+// console.log(nama.replace("a", "o")); //ondi
+// //mengganti semua karakter
+// console.log(nama.replace(/a/g, "o")); //ondi
+// //mengganti semua karakter menjadi huruf besar
+// console.log(nama.toUpperCase()); //ANDI
+// //mengganti semua karakter menjadi huruf kecil
+// console.log(nama.toLowerCase()); //andi
+// //mengganti karakter menjadi huruf besar
+// console.log(nama.replace("a", "A")); //Andi
+// //mengganti karakter menjadi huruf kecil
+// console.log(nama.replace("a", "A")); //Andi
