a7835dc46f9dba805b82be81344551f982f3f085
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 09:51:02 +0700

    tipe data boolean

---
+++ JS_Dasar/boolean.js
@@ -0,0 +1,11 @@
+//tipe data boolean
+// tipe data boolean adalah tipe data yang hanya memiliki dua nilai yaitu true dan false
+// tipe data boolean biasanya digunakan untuk mengecek kondisi
+let benar = true;
+let salah = false;
+console.log(benar);
+console.log(salah);
+// contoh penggunaan boolean
+let angka = 10;
+let hasil = angka > 5;
+console.log(hasil); // hasilnya adalah true karena 10 lebih besar dari 5
