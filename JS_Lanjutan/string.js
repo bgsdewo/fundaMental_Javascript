00c603e83f0b495c4bdc73750564e38cec20409f
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 10:40:06 +0700

    tipe data string

---
+++ JS_Lanjutan/string.js
@@ -0,0 +1,11 @@
+//tipe data string
+//string adalah tipe data yang berisi karakter atau teks
+//string di tulis dengan tanda kutip
+//tanda kutip bisa menggunakan tanda kutip satu atau dua
+//contoh
+let nama = "andi";
+let alamat = "jakarta";
+let pekerjaan = "programmer";
+//penggunaan tanda kutip satu dan dua
+let kalimat = "saya berkata 'halo'";
+let kalimat2 = 'dia berkata "halo"';
