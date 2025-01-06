fffcb6f001a32e91dd6231529e4b49a02d972c09
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 16:52:42 +0700

    Template Literals Save Your Life

---
+++ JS_Lanjutan/literalString.js
@@ -0,0 +1,10 @@
+//Template Literals Save Your Life
+// Template literals adalah string literal yang memungkinkan adanya
+// expression embedded.
+// Template literals menggunakan backtick (`) untuk membungkus string.
+// Template literals dapat mengandung placeholder.
+// Placeholder diwakili oleh ${expression}.
+// Contoh:
+// let nama = "Andi";
+// let umur = 20;
+// console.log(`Halo, nama saya ${nama}, saya berumur ${umur} tahun.`);
