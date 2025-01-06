a1594103f785f9391851c5d529e6a63377ebfd33
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-06 21:42:49 +0700

    operatorOR

---
+++ JS_Logic_Keputusan/operatorNot.js
@@ -0,0 +1,8 @@
+// Operator Logic Not
+// Negasi dari suatu kondisi dapat dilakukan dengan menggunakan operator NOT (!).
+// Operator NOT digunakan untuk membalikkan suatu kondisi.
+// Contoh penggunaan operator NOT:
+// let nilai1 = 10;
+// let nilai2 = 20;
+// let hasil = !(nilai1 > 5);
+// console.log(hasil); // false
