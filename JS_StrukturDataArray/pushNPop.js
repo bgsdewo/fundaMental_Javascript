bc464454362f691ab33fa1692aa3688f61254a51
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-07 21:13:13 +0700

    pushNpop

---
+++ JS_StrukturDataArray/pushNPop.js
@@ -0,0 +1,12 @@
+// method push dan pop pada arrray
+// method push digunakan untuk menambahkan nilai baru pada array
+// method pop digunakan untuk menghapus nilai terakhir pada array
+//penggunaan method push
+var buah = ["apel", "mangga", "jeruk"];
+console.log(buah); //output: ['apel', 'mangga', 'jeruk']
+buah.push("semangka");
+console.log(buah); //output: ['apel', 'mangga', 'jeruk', 'semangka']
+
+//penggunaan method pop
+buah.pop();
+console.log(buah); //output: ['apel', 'mangga', 'jeruk']
