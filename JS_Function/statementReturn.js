8470822848e248bed86d6318449b8f151f201e8b
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-13 11:28:05 +0700

    return statement

---
+++ JS_Function/statementReturn.js
@@ -0,0 +1,11 @@
+//statemen return pada function
+//statement return akan mengakhiri proses di dalam function
+//dan akan memberikan nilai yang didefinisikan pada saat fungsi dipanggil
+
+//definisi function
+function myFunction2(a, b) {
+  return a * b;
+}
+var x = myFunction2(4, 3);
+console.log(x);
+// Output: 12
