de73826fee201d0454fd3d686636698371f92ace
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-13 08:31:17 +0700

    membuat object

---
+++ Struktur_Data_Object/membuatObj.js
@@ -0,0 +1,11 @@
+//cara megakses objek
+
+const dataJohn = {
+  nama: "John",
+  Usia: 30,
+  hobby: ["reading", "cooking"],
+  tempatLahir: {
+    kota: "Jakarta",
+    negara: "Indonesia",
+  },
+};
