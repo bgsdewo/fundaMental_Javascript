caea43cc1f2a311afdd98c3957fc5509f5ff9d31
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-13 13:14:09 +0700

    ekspresi function

---
+++ JS_Function/functionEkspresi.js
@@ -0,0 +1,10 @@
+//funcion ekspresi adalah fungsi yang dideklarasikan dalam bentuk ekspresi
+//fungsi ekspresi dapat disimpan dalam variabel
+//fungsi ekspresi tidak dapat diakses sebelum deklarasi
+//contoh
+
+let myFunction = function (nilai) {
+  return nilai * nilai;
+};
+
+console.log(myFunction(4)); //16
