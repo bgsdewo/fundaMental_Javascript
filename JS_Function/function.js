dd0f739c1fe6cf8635d41ad6ba402bbced2cf9f8
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-13 11:08:08 +0700

    pengenalan function

---
+++ JS_Function/function.js
@@ -0,0 +1,11 @@
+//function dalam javascript
+//function adalah blok kode yang dapat digunakan kembali
+//function dapat digunakan untuk mengembalikan nilai
+//function dapat digunakan tanpa nilai kembali
+//function dapat digunakan dengan atau tanpa parameter
+
+//definisi function
+function myFunction() {
+  console.log("Hello World!");
+}
+myFunction();
