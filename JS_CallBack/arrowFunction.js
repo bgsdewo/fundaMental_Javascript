e6d573f1b697e5b9b51eafe2f5c6fbb783c067d7
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-13 17:10:47 +0700

    arrow function

---
+++ JS_CallBack/arrowFunction.js
@@ -0,0 +1,8 @@
+//arrow Function pada javascript
+//arrow function adalah cara baru dalam penulisan function pada javascript
+
+const perpangkatan = (x) => {
+  return x * x;
+};
+
+console.log(perpangkatan(5));
