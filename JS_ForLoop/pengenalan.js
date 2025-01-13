d21d540e850aed77fda9db55493a7f3a0f1af389
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-13 08:52:45 +0700

    pengenalan perulangan

---
+++ JS_ForLoop/pengenalan.js
@@ -0,0 +1,9 @@
+//pengealan perulangan pada javascript
+//perulangan adalah sebuah proses di mana kita bisa mengeksekusi sebuah kode
+// secara berulang kali
+// perulangan pada javascript terdiri dari for, while, do while
+
+//sintaks perulangan for
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
