2e5e7635eccba8446bc57606949b5a4b49ac42c1
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-13 08:48:01 +0700

    memanggil object dalam array

---
+++ Struktur_Data_Object/ObjInArr.js
@@ -0,0 +1,16 @@
+//cara memanggil objek dalam array
+const shoppingList = [
+  {
+    name: "Apple",
+    qty: 5,
+  },
+  {
+    name: "Banana",
+    qty: 10,
+  },
+  {
+    name: "Cherry",
+    qty: 15,
+  },
+];
+console.log(shoppingList[0].name);
