014880b5babd0b386240954946439326db8edaca
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-13 08:39:03 +0700

    cara mengubah nilai pada obj

--- Struktur_Data_Object/membuatObj.js
+++ Struktur_Data_Object/membuatObj.js
@@ -16,3 +16,11 @@ console.log(dataJohn.Usia);
 console.log(dataJohn.hobby);
 console.log(dataJohn.tempatLahir.kota);
 console.log(dataJohn.tempatLahir.negara);
+
+//cara mengubah nilai objek
+dataJohn.nama = "John Doe";
+console.log(dataJohn.nama);
+
+//cara menambahkan key dan value baru
+dataJohn.pekerjaan = "Guru";
+console.log(dataJohn);
