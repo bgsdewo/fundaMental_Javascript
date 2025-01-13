3f5409e74fb29f1491d5989644c145f5ce4e6db8
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-13 08:23:46 +0700

    pengenalan object

---
+++ Struktur_Data_Object/introObj.js
@@ -0,0 +1,9 @@
+//struktur data object dalam javascript
+// Object adalah sekumpulan properti yang memiliki key dan value
+//mengaksesnya dengan menggunakan custom keys
+// Object adalah kumpulan data dan/atau fungsi yang memiliki nama dan nilai.
+// Object dalam JavaScript adalah entitas independen dengan properti dan tipe.
+// Object adalah instance dari class, dan class adalah blueprint untuk object.
+// Object dapat mewarisi properti dari object lain.
+// Object adalah tipe data yang paling fleksibel.
+// Object dapat berisi fungsi, yang disebut sebagai method object.
