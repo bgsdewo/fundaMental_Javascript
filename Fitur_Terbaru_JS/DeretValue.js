d68e00e3dd170b91f3b1e9bbfc6687a333b181de
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-14 21:26:03 +0700

    spread operator

---
+++ Fitur_Terbaru_JS/DeretValue.js
@@ -0,0 +1,8 @@
+//Mengubah Array Atau Object Menjadi Deret Value Argument Function
+//spread operator pada function
+//mengubah suatu array atau object bahkan string menjadi deret parameter
+//untuk function
+
+const angka = [1, 2, 3, 4, 5];
+
+console.log(Math.max(...angka));
