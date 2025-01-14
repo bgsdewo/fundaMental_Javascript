29418fb66f518bb4182bf0d615915ddba3793395
Bagus Sadewo <bgsdewoo@gmail.com>
2025-01-14 21:33:59 +0700

    merged array

---
+++ Fitur_Terbaru_JS/mergeArr.js
@@ -0,0 +1,8 @@
+//Menggabungkan Nilai Array Dengan Array Lagi
+//menggunakan spread operator
+
+const angka = [1, 2, 3, 4, 5];
+const nama = ["Bagus", "Sadewo"];
+const campuran = [...angka, ...nama];
+
+console.log(campuran);
